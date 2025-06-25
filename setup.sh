e0ac902e800ff2199fbf001680c3e4e6e66be1b4
skyler <102015064+skylerahuman@users.noreply.github.com>
2025-06-25 12:35:16 -0400

    Use alacritty as terminal

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 mkdir -p "$HOME/.config"
-for d in kitty hypr yazi ytmusic-tui ytfzf greetd nvim; do
+for d in alacritty hypr yazi ytmusic-tui ytfzf greetd nvim; do
   mkdir -p "$HOME/.config/$d"
   [ -d "config/$d" ] && for f in config/$d/*; do
     ln -sf "$PWD/$f" "$HOME/.config/$d/"
@@ -12,4 +12,4 @@ done
 ln -sf "$PWD/.zshrc" "$HOME/.zshrc"
 ln -sf "$PWD/.p10k.zsh" "$HOME/.p10k.zsh"
 
-printf '\nDotfiles linked. Install packages: zsh kitty hyprland tuigreet.\n'
+printf '\nDotfiles linked. Install packages: zsh alacritty hyprland tuigreet.\n'
